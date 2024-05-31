0839baed3415843c053ceccd66357556780bdf06
Teslim Olayiwola <olayiwolateslim9@gmail.com>
2024-05-30 23:48:32 -0500

    add homework solution

---
+++ 02-experiment-tracking/homework/Q3/train.py
@@ -0,0 +1,40 @@
+import os
+import pickle
+import click
+import mlflow
+
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error
+
+def load_pickle(filename: str):
+    with open(filename, "rb") as f_in:
+        return pickle.load(f_in)
+
+
+@click.command()
+@click.option(
+    "--data_path",
+    default="./output",
+    help="Location where the processed NYC taxi trip data was saved"
+)
+
+
+def run_train(data_path: str):
+    # set the working path for the mflow
+    mlflow.set_tracking_uri("sqlite:///mlflow.db")
+    # start tracking experiment
+    mlflow.sklearn.autolog()
+    with mlflow.start_run():
+        X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
+        X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
+        max_depth, random_state = 10, 0
+
+        rf = RandomForestRegressor(max_depth=max_depth, random_state=random_state)
+        rf.fit(X_train, y_train)
+        y_pred = rf.predict(X_val)
+
+        rmse = mean_squared_error(y_val, y_pred, squared=False)
+
+
+if __name__ == '__main__':
+    run_train()
